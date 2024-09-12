dbe864d240aac83e24060cc6e61202cc3fda23b4
Nilesh Awachat <nilawachat2001@gmail.com>
2024-09-12 23:14:15 +0530

    navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
+import '../index.css'
+import logo from './logo.png'
 
 const Header = () => {
   return (
-    <div>
-        <div>
-          Logo
-            {/* Nilesh work Here  */}
-        </div>
-
-        <div>
-            Items 
-            {/* Khusbu work Here  */}
-        </div>
+    <div className='header flex justify-between items-center p-4 bg-gray-100 shadow-md'>
+      <div className='logo'>
+        {/* Nilesh work Here */}
+        <img src={logo} alt="Logo" className='h-20 w-auto' />
+      </div>
+      <div className='items'>
+        {/* Khusbu work Here */}
+        <ul className='flex space-x-6 text-lg'>
+          <li className='hover:text-blue-500 cursor-pointer'>Home</li>
+          <li className='hover:text-blue-500 cursor-pointer'>Contact Us</li>
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Header
