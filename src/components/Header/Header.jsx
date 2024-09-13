295cd33159204f66b164c6918d69eb21f698337d
Nilesh Awachat <nilawachat2001@gmail.com>
2024-09-13 12:46:50 +0530

    removed react imports

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,16 +10,16 @@ const Header = () => {
     <div className='header flex justify-between items-center p-4 bg-gray-100 shadow-md'>
       <div className='logo'>
         {/* Nilesh work Here */}
-        <Link to = "/">
-        <img src={logo} alt="Logo" className='h-20 w-auto' />
+        <Link to="/">
+          <img src={logo} alt="Logo" className='h-20 w-auto' />
         </Link>
       </div>
       <div className='items'>
         {/* Khusbu work Here */}
         <ul className='flex space-x-6 text-lg'>
 
-          <li className='hover:text-blue-500 cursor-pointer'><Link to ="/">Home</Link></li>
-          <li className='hover:text-blue-500 cursor-pointer'><Link to ="/contact">Contact Us</Link></li>
+          <li className='hover:text-blue-500 cursor-pointer'><Link to="/">Home</Link></li>
+          <li className='hover:text-blue-500 cursor-pointer'><Link to="/contact">Contact Us</Link></li>
         </ul>
       </div>
     </div>
