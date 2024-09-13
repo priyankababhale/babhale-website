698398d51ab14cc0a715dcbc5dee88047338d93f
Nilesh Awachat <nilawachat2001@gmail.com>
2024-09-13 12:33:35 +0530

    created link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
+// import React from 'react'
 import logo from './logo.png'
+import { Link } from 'react-router-dom';
+// import logo from './path/to/logo.png'
+
+
 
 const Header = () => {
   return (
     <div className='header flex justify-between items-center p-4 bg-gray-100 shadow-md'>
       <div className='logo'>
         {/* Nilesh work Here */}
+        <Link to = "/">
         <img src={logo} alt="Logo" className='h-20 w-auto' />
+        </Link>
       </div>
       <div className='items'>
         {/* Khusbu work Here */}
         <ul className='flex space-x-6 text-lg'>
-          <li className='hover:text-blue-500 cursor-pointer'>Home</li>
-          <li className='hover:text-blue-500 cursor-pointer'>Contact Us</li>
+
+          <li className='hover:text-blue-500 cursor-pointer'><Link to ="/">Home</Link></li>
+          <li className='hover:text-blue-500 cursor-pointer'><Link to ="/contact">Contact Us</Link></li>
         </ul>
       </div>
     </div>
