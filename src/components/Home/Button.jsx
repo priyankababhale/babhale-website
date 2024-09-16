5a01e09a57ecab0aa047cb39a936278056ca79f6
smdekate <smdekate@gmail.com>
2024-09-16 15:47:29 +0530

    button component done

--- src/components/Home/Button.jsx
+++ src/components/Home/Button.jsx
@@ -1,6 +1,11 @@
-const Button = () {
+const Button = ({ background = "#FF2D2D", color = "#FFF", text = "Read more", border = "0px solid black" }) => {
   return (
-    <div>Button</div>
+    <button
+      style={{backgroundColor: background, color: color, border: border}}
+      className={`py-2 px-4 font-medium`}
+    >
+      {text}
+    </button>
   )
 }
 
