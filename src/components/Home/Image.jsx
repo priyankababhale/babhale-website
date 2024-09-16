8b62cc2f363c16cb4707fccc491beef417ed3b15
priyankababhale <72650574+priyankababhale@users.noreply.github.com>
2024-09-16 14:30:20 +0530

    Update Image.jsx

--- src/components/Home/Image.jsx
+++ src/components/Home/Image.jsx
@@ -3,12 +3,12 @@
 const Image = ({url, height, width, alt="image" }) => {
   return (
     <div style={{width:width, height:height}}>
-    <img
-      src={url}
-      alt={alt}
-      width="100%"
-      height="100%"
-    />
+      <img
+        src={url}
+        alt={alt}
+        width="100%"
+        height="100%"
+      />
     </div>
   )
 }
