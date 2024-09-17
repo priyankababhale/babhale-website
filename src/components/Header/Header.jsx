6f5a6ef935ad29ebeacf264a49ea23cd35cdf67f
Nilesh Awachat <nilawachat2001@gmail.com>
2024-09-17 21:34:46 +0530

    worked on section and added images + icons in assets

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
-import logo from './logo.png'
+import logo from '../../assets/images/logo.png'
 import { Link } from 'react-router-dom';
 // import logo from './path/to/logo.png'
 
 const Header = () => {
   return (
+    
     <div className='header flex justify-between items-center p-4 bg-gray-100 shadow-md'>
       <div className='logo'>
         {/* Nilesh work Here */}
@@ -11,11 +12,13 @@ const Header = () => {
           <img src={logo} alt="Logo" className='h-20 w-auto' />
         </Link>
       </div>
+
+
       <div className='items'>
         {/* Khusbu work Here */}
         <ul className='flex space-x-6 text-lg'>
 
-          <li className='hover:text-blue-500 cursor-pointer'><Link to="/">Home</Link></li>
+          <li className='hover:text-blue-500 cursor-pointer '><Link to="/">Home</Link></li>
           <li className='hover:text-blue-500 cursor-pointer'><Link to="/contact">Contact Us</Link></li>
         </ul>
       </div>
--- src/components/Home/Section1.jsx
+++ src/components/Home/Section1.jsx
@@ -1,6 +1,41 @@
+import Button from './Button'
+import Image from './Image'
 const Section1 = () => {
   return (
-    <>Section 1</>
+    <>
+    <div style={{ border: '1px solid black' }} ></div>
+      <div className='flex flex-wrap justify-around ..'>
+
+        <div className='relative h-64' >
+           <div className="m-8 ... p-40 ... font-medium text-5xl  text-center"><p>Web and <span className='orange-text'>Mobile</span> App <br/> Development <span className='orange-text'>Company</span>
+           </p></div>
+
+          <div className='flex justify-center items-center text-2xl -mt-40'>
+            <Button />
+          </div>
+        
+        </div>
+
+
+        <div>
+           <Image
+            url={'src/assets/images/image1.png'}
+            width="601px"
+            height="484px"
+          />
+        </div>
+
+      </div>
+
+      <div className='m-20 font-medium text-4xl text-center'>
+        <div className="border-b-2 border-black mb-4" ></div>
+        We’re a Worldide <span className="text-orange-500 font-bold">IT Company </span>changing how the World’s greatest brands interface with clients and
+        <span className="text-orange-500 font-bold"> develop</span>their business with Babhale, you get <span className='orange-text text-orange-500 font-bold'>insight and skill</span>, speed and deftness,
+        and a solid portion of <span className="text-orange-500 font-bold">sober-mindedness</span> to drive your Business forward.
+        <div className="border-b-2 border-black mt-4"></div>
+      </div>
+
+    </>
   )
 }
 
