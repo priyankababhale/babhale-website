ed9aed3bb201f227f36f790feda2874c4fb62560
NikitaBhoyar <nikitabhoyar005@gmail.com>
2024-09-18 16:36:38 +0530

    section 4 completed

--- src/components/Home/Section4.jsx
+++ src/components/Home/Section4.jsx
@@ -9,7 +9,7 @@ const Section4 = () => {
 
     <>
       <SectionHead
-        text_center = "true"
+        text_center="true"
         heading="Sucessfully completed 100+ projects with numbers of satisfied client"
       />
 
@@ -25,32 +25,20 @@ const Section4 = () => {
           />
         </div>
 
-        
-        {/* <div className=' ml-100 h-601  w-640 top-4561 gap-80 mt-12' > */}
-          {/* <h2 className='font-bold text-4xl  p-8 font-roboto'>We have a Creative Team Discover  <br />
+        <div className=' ml-100 h-601  w-640 top-4561 gap-80 mt-12'>
+
+
+          <h2 className='font-bold text-4xl  p-8' >We have a Creative Team Discover  <br />
             our experts</h2>
-
-          <p style={{ color: "#75706D", padding: "8px", marginLeft: "35px", fontSize: "20px" }}>Create some exclusive way to solve our customer problems</p>
-
-          <h1 className='font-bold text-2xl p-10'>Our team has expertise with UI/UX designing which can <br />
-            help our customer to know how will product will look like <br />
-            before going to development phase..</h1> */}
-
-
-<div className=' ml-100 h-601  w-640 top-4561 gap-80 mt-12'>
-
-
-            <h2  className='font-bold text-4xl  p-8' >We have a Creative Team Discover  <br />
-            our experts</h2>
-            <p className='font-medium ml-8' style={{ color: "#75706D", fontSize:"25px" }}>Create some exclusive way to solve our customer <br />problems</p>
-            <h1 className='font-bold text-2xl p-8' style={{fontSize: "25px"}} >Our team has expertise with UI/UX designing which can <br />
+          <p className='font-medium ml-8' style={{ color: "#75706D", fontSize: "25px" }}>Create some exclusive way to solve our customer <br />problems</p>
+          <h1 className='font-bold text-2xl p-8' style={{ fontSize: "25px" }} >Our team has expertise with UI/UX designing which can <br />
             help our customer to know how will product will look like <br />
             before going to development phase..</h1>
-            <div className='inline-block pl-8' style={{fontSize: "25px"}}>
-            <Button 
+          <div className='inline-block pl-8' style={{ fontSize: "25px" }}>
+            <Button
               text="Let Us Know"
             />
-            </div>
+          </div>
         </div>
 
       </div>
