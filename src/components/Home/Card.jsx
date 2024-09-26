18f23635a76b26486314a12dcf73e0ace2238968
Nilesh Awachat <nilawachat2001@gmail.com>
2024-09-26 18:27:51 +0530

    contactus page half done

--- src/components/Home/Card.jsx
+++ src/components/Home/Card.jsx
@@ -7,8 +7,8 @@ const Card = ({ iconUrl, title, content, width, iconWidth, iconHeight }) => {
       style={{ width: width }}
     >
       <img src={iconUrl} alt="icon" width={iconWidth} height={iconHeight} />  {/* there's a bug here, icon is not rendering */}
-      <h3 className='font-medium text-2xl mt-1 mb-2'>{title}</h3>
-      <p style={{color: "#75706D"}} className='font-medium text-xl'>{content}</p>
+      <h3 className=' text-2xl mt-1 mb-2 font-[Adamina]'>{title}</h3>
+      <p style={{color: "#75706D"}} className=' text-xl font-[Adamina]'>{content}</p>
     </div>
   )
 }
