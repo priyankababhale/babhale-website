29e9d4b62e24c77b52e5a8f7bf115d66cdce2aa9
NikitaBhoyar <nikitabhoyar005@gmail.com>
2024-10-03 16:29:54 +0530

    condtionally rendering button

--- src/components/Home/SectionHead.jsx
+++ src/components/Home/SectionHead.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import Button from "./Button"
 
-const SectionHead = ({text_center = false, heading}) => {
+const SectionHead = ({text_center = false, heading, btn_flag = false}) => {
   return (
-    <div className={`w-full section-header ${text_center === 'true' ? 'text-center' : {}}`}>
+    <div className={`w-full section-header ${text_center && 'text-center'}`}>
       <h2>{heading}</h2>
+
+      {btn_flag && <Button />}
     </div>
   )
 }
